5e1b7c17ab4aa756c5fea2778db7bbd8dbdc915c
Ramiro Gonzalez <rgonzalez69@ucmerced.edu>
2018-11-26 14:58:31 -0800

    Add files via upload

---
+++ Projects/11-29-2018/11-29-2018.ino
@@ -0,0 +1,17 @@
+//This is a comment 
+//Any variables created here are global, they are accessible everywhere in the program
+int globalVar = 5; //A global variable of integer data type initialized to 5. 
+int gobalVar2; //A global variable declared but not initialized. Definition without initialization
+void setup() {
+  // put your setup code here, to run once:
+  //This code will run only when arduino boots. 
+  globalVar2 = 5; /*Initalized. If globalVar2 had
+  a previous value, it is assigned a 5 here. */
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //As he name implies this continues to execute
+
+}
